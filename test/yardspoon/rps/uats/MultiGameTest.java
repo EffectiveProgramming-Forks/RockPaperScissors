5b8a4185c608342f03e46e3a47de60aad1b13c41
Benjamin P Lee <leebenjp@gmail.com>
2010-01-13 20:37:18 -0600

    UAT: starting phase 2 with 'best-of X'

---
+++ test/yardspoon/rps/uats/MultiGameTest.java
@@ -0,0 +1,67 @@
+package yardspoon.rps.uats;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import org.junit.Test;
+
+import yardspoon.rps.RockPaperScissors;
+
+public class MultiGameTest {
+	
+	@Test
+	public void firstToThreeWithPlayerTwoWinningTwoGames() throws Exception {
+		String input = "Amos\nKenny\nS\nR\nS\nR\nS\nR\n";
+		String expectedOutput = "Player 1 Name: " +
+								"Player 2 Name: " +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"Kenny Wins! (0-1)" + System.getProperty("line.separator") +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"Kenny Wins! (0-2)" + System.getProperty("line.separator") +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"Kenny Wins! (0-3)" + System.getProperty("line.separator");
+		
+		String actualOutput = executeGame(input, "-to", "3");
+		
+		assertEquals(expectedOutput, actualOutput);
+	}
+	
+	@Test
+	public void firstToTwoWithPlayerOneWinningOneGamesAndPlayerTwoWinningOneGame() throws Exception {
+		String input = "Amos\nKenny\nS\nP\nR\nP\nS\nP\n";
+		String expectedOutput = "Player 1 Name: " +
+								"Player 2 Name: " +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"Amos Wins! (1-0)" + System.getProperty("line.separator") +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"Kenny Wins! (1-1)" + System.getProperty("line.separator") +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"Amos Wins! (2-1)" + System.getProperty("line.separator");
+		
+		String actualOutput = executeGame(input, "-to", "2");
+		
+		assertEquals(expectedOutput, actualOutput);
+	}
+	
+	private String executeGame(String input, String... arguments) throws IOException {
+		PrintStream originalOutputStream = System.out;
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outputStream));
+		System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+		RockPaperScissors.main(arguments);
+		
+		System.setOut(originalOutputStream);
+		return outputStream.toString();
+	}
+}
