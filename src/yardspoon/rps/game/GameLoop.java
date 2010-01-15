e68cc2db07421df99825d811aed736401982619a
Benjamin P Lee <leebenjp@gmail.com>
2010-01-14 22:27:16 -0600

    Red:Green:Repeat: fixed game loop tests

--- src/yardspoon/rps/game/GameLoop.java
+++ src/yardspoon/rps/game/GameLoop.java
@@ -3,6 +3,7 @@ package yardspoon.rps.game;
 import java.io.IOException;
 
 import yardspoon.rps.ui.InterfaceModifier;
+import yardspoon.rps.ui.MultiGameContinuation;
 import yardspoon.rps.ui.ThrowTranslator;
 import yardspoon.rps.ui.UserInterface;
 
@@ -10,17 +11,44 @@ public class GameLoop {
 
 	public void play(UserInterface userInterface, InterfaceModifier interfaceModifier,
 			ThrowTranslator throwTranslator) throws IOException {
+
+		String player1Name = userInterface.prompt(interfaceModifier.getPlayer1NamePrompt());
+		String player2Name = userInterface.prompt(interfaceModifier.getPlayer2NamePrompt());
+		
+		Result result = playSingleGame(userInterface, interfaceModifier, throwTranslator);
+		
+		userInterface.inform(interfaceModifier.getOutcome(player1Name, player2Name, result));
+	}
+
+	private Result playSingleGame(UserInterface userInterface,
+			InterfaceModifier interfaceModifier, ThrowTranslator throwTranslator)
+			throws IOException {
+		String player1ThrowName = userInterface.prompt(interfaceModifier.getThrowPrompt());
+		String player2ThrowName = userInterface.prompt(interfaceModifier.getThrowPrompt());
+	
+		Throw player1Throw = throwTranslator.translate(player1ThrowName);
+		Throw player2Throw = throwTranslator.translate(player2ThrowName);
+	
+		Result result = player1Throw.play(player2Throw);
+		return result;
+	}
+
+	public void play(UserInterface userInterface,
+			InterfaceModifier interfaceModifier,
+			ThrowTranslator throwTranslator,
+			MultiGameContinuation multiGameContinuation) throws IOException {
 		
 		String player1Name = userInterface.prompt(interfaceModifier.getPlayer1NamePrompt());
 		String player2Name = userInterface.prompt(interfaceModifier.getPlayer2NamePrompt());
 		
-		String player1ThrowName = userInterface.prompt(interfaceModifier.getThrowPrompt());
-		String player2ThrowName = userInterface.prompt(interfaceModifier.getThrowPrompt());
-		
-		Throw player1Throw = throwTranslator.translate(player1ThrowName);
-		Throw player2Throw = throwTranslator.translate(player2ThrowName);
-		
-		userInterface.inform(interfaceModifier.getOutcome(player1Name, player2Name, player1Throw.play(player2Throw)));
+		do {
+			Result result = playSingleGame(userInterface, interfaceModifier, throwTranslator);
+			
+			userInterface.inform(interfaceModifier.getOutcome(player1Name, player2Name, result));
+			
+			multiGameContinuation.addPlayerOneResult(result);
+			userInterface.inform(multiGameContinuation.report());
+		} while (!multiGameContinuation.stopPlaying());
 	}
 
 }
--- test/yardspoon/rps/game/GameLoopTest.java
+++ test/yardspoon/rps/game/GameLoopTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.when;
 import org.junit.Test;
 
 import yardspoon.rps.ui.InterfaceModifier;
+import yardspoon.rps.ui.MultiGameContinuation;
 import yardspoon.rps.ui.ThrowTranslator;
 import yardspoon.rps.ui.UserInterface;
 
@@ -47,5 +48,53 @@ public class GameLoopTest {
 		verify(mockUserInterface).prompt(player2Prompt);
 		verify(mockUserInterface, times(2)).prompt(throwPrompt);
 		verify(mockUserInterface).inform(outcome);
+	}
+	
+	@Test
+	public void firstToXGameLoopCallsCorrectMethod() throws Exception {
+		String player1Prompt = "Prompt 1";
+		String player1Name = "P 1";
+		String player2Prompt = "Prompt 2";
+		String player2Name = "P 2";
+		String throwPrompt = "Throw";
+		String player1Throw = "A";
+		String player2Throw = "B";
+		String outcome = "Outcome";
+		String firstRecord = "Record: 1-0";
+		String finalRecord = "Final Record: 2-0";
+		Result result = Result.Win;
+		
+		UserInterface mockUserInterface = mock(UserInterface.class);
+		when(mockUserInterface.prompt(player1Prompt)).thenReturn(player1Name);
+		when(mockUserInterface.prompt(player2Prompt)).thenReturn(player2Name);
+		when(mockUserInterface.prompt(throwPrompt)).thenReturn(player1Throw, player2Throw);
+		
+		InterfaceModifier mockInterfaceModifier = mock(InterfaceModifier.class);
+		when(mockInterfaceModifier.getPlayer1NamePrompt()).thenReturn(player1Prompt);
+		when(mockInterfaceModifier.getPlayer2NamePrompt()).thenReturn(player2Prompt);
+		when(mockInterfaceModifier.getThrowPrompt()).thenReturn(throwPrompt);
+		when(mockInterfaceModifier.getOutcome(player1Name, player2Name, result)).thenReturn(outcome);
+		
+		ThrowTranslator mockThrowTranslator = mock(ThrowTranslator.class);
+		when(mockThrowTranslator.translate(player1Throw)).thenReturn(Throw.Rock);
+		when(mockThrowTranslator.translate(player2Throw)).thenReturn(Throw.Scissors);
+		
+		MultiGameContinuation mockMultiGameContinuation = mock(MultiGameContinuation.class);
+		when(mockMultiGameContinuation.stopPlaying()).thenReturn(false, true);
+		when(mockMultiGameContinuation.report()).thenReturn(firstRecord, finalRecord);
+		
+		GameLoop loop = new GameLoop();
+		loop.play(mockUserInterface, mockInterfaceModifier, mockThrowTranslator, mockMultiGameContinuation);
+		
+		verify(mockUserInterface).prompt(player1Prompt);
+		verify(mockUserInterface).prompt(player2Prompt);
+		verify(mockUserInterface, times(4)).prompt(throwPrompt); // 2 for each game
+		verify(mockUserInterface).inform(outcome);
+		verify(mockMultiGameContinuation).addPlayerOneResult(result);
+		verify(mockUserInterface).inform(firstRecord);
+		verify(mockUserInterface).inform(outcome);
+		verify(mockMultiGameContinuation).addPlayerOneResult(result);
+		verify(mockUserInterface).inform(finalRecord);
+		
 	}
 }
