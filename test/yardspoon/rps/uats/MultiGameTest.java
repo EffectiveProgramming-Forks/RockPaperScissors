aed8ed207564954b4c3fc8bdb612a18e74b451b5
Benjamin P Lee <leebenjp@gmail.com>
2010-01-14 22:05:28 -0600

    UATs: cleaned up tests and added one for ties

--- test/yardspoon/rps/uats/MultiGameTest.java
+++ test/yardspoon/rps/uats/MultiGameTest.java
@@ -21,15 +21,15 @@ public class MultiGameTest {
 								"[R]ock, [P]aper, or [S]cissors? " +
 								"[R]ock, [P]aper, or [S]cissors? " +
 								"Kenny Wins!" + System.getProperty("line.separator") +
-								"Record So Far: Amos-Kenny 0-1" + System.getProperty("line.separator") +
+								"Record: 0-1" + System.getProperty("line.separator") +
 								"[R]ock, [P]aper, or [S]cissors? " +
 								"[R]ock, [P]aper, or [S]cissors? " +
 								"Kenny Wins!" + System.getProperty("line.separator") +
-								"Record So Far: Amos-Kenny 0-2" + System.getProperty("line.separator") +
+								"Record: 0-2" + System.getProperty("line.separator") +
 								"[R]ock, [P]aper, or [S]cissors? " +
 								"[R]ock, [P]aper, or [S]cissors? " +
 								"Kenny Wins!" + System.getProperty("line.separator") +
-								"Record So Far: Amos-Kenny 0-3" + System.getProperty("line.separator");
+								"Final Record: 0-3" + System.getProperty("line.separator");
 		
 		String actualOutput = executeGame(input, "-to", "3");
 		
@@ -44,22 +44,51 @@ public class MultiGameTest {
 								"[R]ock, [P]aper, or [S]cissors? " +
 								"[R]ock, [P]aper, or [S]cissors? " +
 								"Amos Wins!" + System.getProperty("line.separator") +
-								"Record So Far: Amos-Kenny 1-0" + System.getProperty("line.separator") +
+								"Record: 1-0" + System.getProperty("line.separator") +
 								"[R]ock, [P]aper, or [S]cissors? " +
 								"[R]ock, [P]aper, or [S]cissors? " +
-								"Kenny Wins! (1-1)" + System.getProperty("line.separator") +
-								"Record So Far: Amos-Kenny 1-1" + System.getProperty("line.separator") +
+								"Kenny Wins!" + System.getProperty("line.separator") +
+								"Record: 1-1" + System.getProperty("line.separator") +
 								"[R]ock, [P]aper, or [S]cissors? " +
 								"[R]ock, [P]aper, or [S]cissors? " +
-								"Amos Wins! (2-1)" + System.getProperty("line.separator") + 
-								"Amos Wins Overall! Amos-Kenny 2-1" + System.getProperty("line.separator");
+								"Amos Wins!" + System.getProperty("line.separator") + 
+								"Final Record: 2-1" + System.getProperty("line.separator");
 		
 		String actualOutput = executeGame(input, "-to", "2");
 		
 		assertEquals(expectedOutput, actualOutput);
 	}
 	
-	// TODO Add test for ties
+	@Test
+	public void firstToTwoIsNotAlteredByTies() throws Exception {
+		String input = "Amos\nKenny\nS\nP\nR\nP\nR\nR\nR\nR\nS\nP\n";
+		String expectedOutput = "Player 1 Name: " +
+								"Player 2 Name: " +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"Amos Wins!" + System.getProperty("line.separator") +
+								"Record: 1-0" + System.getProperty("line.separator") +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"Kenny Wins!" + System.getProperty("line.separator") +
+								"Record: 1-1" + System.getProperty("line.separator") +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"Players Tie!" + System.getProperty("line.separator") +
+								"Record: 1-1" + System.getProperty("line.separator") +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"Players Tie!" + System.getProperty("line.separator") +
+								"Record: 1-1" + System.getProperty("line.separator") +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"[R]ock, [P]aper, or [S]cissors? " +
+								"Amos Wins!" + System.getProperty("line.separator") + 
+								"Final Record: 2-1" + System.getProperty("line.separator");
+		
+		String actualOutput = executeGame(input, "-to", "2");
+		
+		assertEquals(expectedOutput, actualOutput);
+	}
 	
 	private String executeGame(String input, String... arguments) throws IOException {
 		PrintStream originalOutputStream = System.out;
