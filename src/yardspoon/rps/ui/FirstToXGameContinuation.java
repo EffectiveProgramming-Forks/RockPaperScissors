65d955812021c2049c8359d3b99795c2aaa241a0
Benjamin P Lee <leebenjp@gmail.com>
2010-01-14 22:09:17 -0600

    Red:Green:Repeat tests for First To X multi game type

--- src/yardspoon/rps/ui/FirstToXGameContinuation.java
+++ src/yardspoon/rps/ui/FirstToXGameContinuation.java
@@ -6,6 +6,7 @@ public class FirstToXGameContinuation {
 
 	private final Integer goalNumberOfWins;
 	private Integer numberOfPlayerOneWins = 0;
+	private Integer numberOfPlayerTwoWins = 0;
 
 	public FirstToXGameContinuation(Integer goalNumberOfWins) {
 		this.goalNumberOfWins = goalNumberOfWins;
@@ -15,10 +16,24 @@ public class FirstToXGameContinuation {
 		if(playerOneResult == Result.Win) {
 			numberOfPlayerOneWins++;
 		}
+		else if(playerOneResult == Result.Loss) {
+			numberOfPlayerTwoWins++;
+		}
 	}
 
 	public Boolean stopPlaying() {
-		return numberOfPlayerOneWins == goalNumberOfWins;
+		return 	numberOfPlayerOneWins == goalNumberOfWins || 
+			   	numberOfPlayerTwoWins == goalNumberOfWins;
+	}
+
+	public String report() {
+		String report = "Record: " + numberOfPlayerOneWins + "-" + numberOfPlayerTwoWins;
+		
+		if(stopPlaying()) {
+			report = "Final " + report;
+		}
+		
+		return report;
 	}
 
 }
